b9d16362d6bfeecdc2911dcb0244f83d86c984c3
Eki Iman Jordiansyah <ekiimanjordiansyah212@gmail.com>
2025-04-06 17:23:24 +0700

    preparation for new branch on repo

--- src/components/common/BackWithTitle.jsx
+++ src/components/common/BackWithTitle.jsx
@@ -1,21 +1,21 @@
 import Grid from "@mui/material/Grid2";
-import {Button} from "@mui/material";
-import {ArrowBack} from "@mui/icons-material";
+import { Button } from "@mui/material";
+import { ArrowBack } from "@mui/icons-material";
 import Typography from "@mui/material/Typography";
 import PropTypes from "prop-types";
-import {useNavigate} from "react-router";
+import { useNavigate } from "react-router";
 
-export default function BackWithTitle({title, icon}) {
+export default function BackWithTitle({ title, icon }) {
   const navigate = useNavigate();
   return (
     <>
       <Grid container spacing={2} sx={{ display: "flex", justifyContent: "flex-start", mb: 3, alignItems: "center" }}>
-        <Grid size={{ xs: 3/2, sm: 2, md: 1 }}>
+        <Grid size={{ xs: 3 / 2, sm: 2, md: 1 }}>
           <Button fullWidth variant="contained" color="cbtPrimary"
-                  onClick={async () => await navigate(-1)}
-                  startIcon={<ArrowBack />}> Kembali</Button>
+            onClick={async () => await navigate(-1)}
+            startIcon={<ArrowBack />}> Kembali</Button>
         </Grid>
-        <Typography variant='h5' sx={{ alignItems: 'center'}}>
+        <Typography variant='h5' sx={{ alignItems: 'center' }}>
           {icon} {title}
         </Typography>
       </Grid>
