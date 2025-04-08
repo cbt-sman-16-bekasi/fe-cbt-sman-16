65bac1b5fe3d6f7e9d5d3c6a555cdef8755ab670
Muhammad Suryono <muhammad.suryono@ist.id>
2025-04-08 12:40:05 +0700

    fix: app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Header from './components/Header';
 import SideMenu from './components/SideMenu';
 import AppTheme from '../shared-theme/AppTheme';
 import { Route, Routes, useLocation, useNavigate } from 'react-router';
-import { useEffect } from 'react';
+import {useEffect, useState} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import SuperAdminPage from "./pages/SuperAdminPage";
@@ -27,7 +27,7 @@ import ProtectedRoute from './components/ProtectedRoute';
 import NotFoundPage from './pages/NotFoundPage';
 import { asyncPreloadProcess } from './states/isPreload/action.js';
 import { asyncUnsetAuthUser } from './states/authUser/action.js';
-// import Typography from "@mui/material/Typography";
+import Typography from "@mui/material/Typography";
 
 const xThemeComponents = {
   ...chartsCustomizations,
@@ -40,7 +40,7 @@ function App(props) {
   const authUser = useSelector((state) => state.authUser);
   const isPreload = useSelector((state) => state.isPreload);
   const accessToken = localStorage.getItem("accessToken");
-  // const [title, setTitle] = useState('')
+  const [title, setTitle] = useState('')
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -58,147 +58,115 @@ function App(props) {
     teacher: <TeacherPage role={userRole} />,
   };
 
-  // useEffect(() => {
-  //   const currentMenu = JSON.parse(localStorage.getItem("currentMenu"));
-  //   setTitle(currentMenu === null ? 'Dashboard' : currentMenu.title);
+  useEffect(() => {
+    const currentMenu = JSON.parse(localStorage.getItem("currentMenu"));
+    setTitle(currentMenu === null ? 'Dashboard' : currentMenu.title);
+  }, [navigate]);
 
-  //   if (!authUser || !accessToken) {
-  //     navigate("/login");
-  //   }
-  // }, [authUser, accessToken, navigate]);
+  useEffect(() => {
+    if (!accessToken) {
+      localStorage.clear()
+      navigate("/login");
+    }
+  }, [accessToken]);
 
-  // useEffect(() => {
-  //   const currentMenu = JSON.parse(localStorage.getItem("currentMenu"));
-  //   setTitle(currentMenu === null ? 'Dashboard' : currentMenu.title);
-  // }, [navigate]);
-
-  // useEffect(() => {
-  //   if (!accessToken) {
-  //     localStorage.clear()
-  //     navigate("/login");
-  //   }
-  // }, [accessToken]);
-
-  // populate data
   useEffect(() => {
     dispatch(asyncPreloadProcess());
   }, [dispatch]);
 
-  // navigate
   useEffect(() => {
-    if (isPreload) return;
-
-    if (!authUser || !accessToken) {
-      if (location.pathname !== '/login' && location.pathname !== '/register') {
-        navigate("/login");
-      }
-      return;
-    }
-
     if (userRole) {
-      const currentPath = location.pathname;
-      const rolePath = `/${userRole}`;
+      const role = userRole;
+      const path = window.location.pathname;
 
-      if (!currentPath.startsWith(rolePath)) {
-        // Jika di root atau auth pages
-        if (currentPath === "/" || currentPath === "/login" || currentPath === "/register") {
-          navigate(`${rolePath}/dashboard`);
-        } else {
-          navigate('/not-found');
-        }
+      if (path === "/" || path === "/login" || path === "/register") {
+        navigate(`/${role}/dashboard`);
       }
     }
-  }, [authUser, accessToken, userRole, navigate, location, isPreload]);
-  // }, [userRole]);
+  }, [userRole]);
 
-  // title tab
   useEffect(() => {
-    if (userRole && authUser) {
-      const pathParts = location.pathname.split('/');
-      const currentSubPath = pathParts.slice(2).join('/') || "/";
-      const userMenu = menuConfig[userRole] || [];
-      const currentMenu = userMenu.find((item) => item.path === `/${currentSubPath}`);
+    const currentPath = location.pathname.split('/').slice(2).join('/') || "/";
+    const userMenu = menuConfig[userRole] || [];
+    const currentMenu = userMenu.find((item) => item.path === `/${currentPath}`);
 
-      document.title = currentMenu ? `${currentMenu.text} - Admin` : "Admin Panel";
-    } else {
-      document.title = "Admin Panel";
-    }
-  }, [location, userRole, authUser]);
+    document.title = currentMenu ? `${currentMenu.text} - Admin` : "Admin Panel";
+  }, [location, userRole]);
 
   if (isPreload) null
 
   if (!authUser || !accessToken) {
     return (
-      <>
-        <AppTheme {...props} themeComponents={xThemeComponents}>
-          <CssBaseline enableColorScheme />
-          <Box component="main"
-            sx={{
-              margin: 0,
-              height: "100vh",
-              width: "100vw",
-            }} >
-            <Routes>
-              <Route path='/login' element={<LoginPage />} />
-              <Route path='/register' element={<RegisterPage />} />
-            </Routes>
-          </Box >
-        </AppTheme>
-      </>
+        <>
+          <AppTheme {...props} themeComponents={xThemeComponents}>
+            <CssBaseline enableColorScheme />
+            <Box component="main"
+                 sx={{
+                   margin: 0,
+                   height: "100vh",
+                   width: "100vw",
+                 }} >
+              <Routes>
+                <Route path='/login' element={<LoginPage />} />
+                <Route path='/register' element={<RegisterPage />} />
+              </Routes>
+            </Box >
+          </AppTheme>
+        </>
     )
   }
 
   return (
-    <div className="app w-[98.5vw]">
-      <AppTheme {...props} themeComponents={xThemeComponents}>
-        <CssBaseline enableColorScheme />
-        <Box sx={{ display: 'flex', width: '100%' }}>
-          <SideMenu user={authUser} role={userRole} logout={onUserLogout} />
-          <AppNavbar user={authUser} role={userRole} logout={onUserLogout} />
+      <div className="app w-[98.5vw]">
+        <AppTheme {...props} themeComponents={xThemeComponents}>
+          <CssBaseline enableColorScheme />
+          <Box sx={{ display: 'flex', width: '100%' }}>
+            <SideMenu user={authUser} role={userRole} logout={onUserLogout} />
+            <AppNavbar user={authUser} role={userRole} logout={onUserLogout} />
 
-          <Box
-            component="main"
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              flexGrow: 1,
-              backgroundColor: (theme) =>
-                theme.vars
-                  ? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
-                  : alpha(theme.palette.background.default, 1),
-              overflow: 'auto',
-              minHeight: '100vh',
-            }}
-          >
-            <Stack
-              sx={{
-                alignItems: 'start',
-                mx: 3,
-                pb: 5,
-                mt: { xs: 8, md: 0 },
-              }}
+            <Box
+                component="main"
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  flexGrow: 1,
+                  backgroundColor: (theme) =>
+                      theme.vars
+                          ? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
+                          : alpha(theme.palette.background.default, 1),
+                  overflow: 'auto',
+                  minHeight: '100vh',
+                }}
             >
-              <Header role={userRole} />
+              <Stack
+                  sx={{
+                    alignItems: 'start',
+                    mx: 3,
+                    pb: 5,
+                    mt: { xs: 8, md: 0 },
+                  }}
+              >
+                <Header role={userRole} />
 
-              <Routes>
-                <Route
-                  path={`/${userRole}/*`}
-                  element={
-                    <ProtectedRoute allowedRoles={[userRole]} userRole={userRole}>
-                      {/* <Typography component="h1" variant="h4" fontWeight="bold">
-                        {title}
-                      </Typography> */}
-                      {routeMap[userRole]}
-                    </ProtectedRoute>
-                  } />
-                <Route path="*" element={<NotFoundPage role={userRole} />} />
-              </Routes>
+                <Routes>
+                  <Route
+                      path={`/${userRole}/*`}
+                      element={
+                        <ProtectedRoute allowedRoles={[userRole]} userRole={userRole}>
+                          <Typography component="h1" variant="h4" fontWeight="bold">
+                            {title}
+                          </Typography>
+                          {routeMap[userRole]}
+                        </ProtectedRoute>
+                      } />
+                  <Route path="*" element={<NotFoundPage role={userRole} />} />
+                </Routes>
 
-            </Stack>
+              </Stack>
+            </Box>
           </Box>
-        </Box>
-      </AppTheme>
-    </div>
+        </AppTheme>
+      </div>
   );
 }
 
