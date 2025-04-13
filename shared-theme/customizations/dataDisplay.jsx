a4f22f906d7e1bc5f095102d12a6199be4f6124a
Eki Iman Jordiansyah <ekiimanjordiansyah212@gmail.com>
2025-04-13 21:05:07 +0700

    hiding teacher menu first

--- shared-theme/customizations/dataDisplay.jsx
+++ shared-theme/customizations/dataDisplay.jsx
@@ -97,105 +97,105 @@ export const dataDisplayCustomizations = {
         [`& .${chipClasses.label}`]: {
           fontWeight: 600,
         },
-        variants: [
-          {
-            props: {
-              color: 'default',
-            },
-            style: {
-              borderColor: gray[200],
-              backgroundColor: gray[100],
-              [`& .${chipClasses.label}`]: {
-                color: gray[500],
-              },
-              [`& .${chipClasses.icon}`]: {
-                color: gray[500],
-              },
-              ...theme.applyStyles('dark', {
-                borderColor: gray[700],
-                backgroundColor: gray[800],
-                [`& .${chipClasses.label}`]: {
-                  color: gray[300],
-                },
-                [`& .${chipClasses.icon}`]: {
-                  color: gray[300],
-                },
-              }),
-            },
-          },
-          {
-            props: {
-              color: 'success',
-            },
-            style: {
-              borderColor: green[200],
-              backgroundColor: green[50],
-              [`& .${chipClasses.label}`]: {
-                color: green[500],
-              },
-              [`& .${chipClasses.icon}`]: {
-                color: green[500],
-              },
-              ...theme.applyStyles('dark', {
-                borderColor: green[800],
-                backgroundColor: green[900],
-                [`& .${chipClasses.label}`]: {
-                  color: green[300],
-                },
-                [`& .${chipClasses.icon}`]: {
-                  color: green[300],
-                },
-              }),
-            },
-          },
-          {
-            props: {
-              color: 'error',
-            },
-            style: {
-              borderColor: red[100],
-              backgroundColor: red[50],
-              [`& .${chipClasses.label}`]: {
-                color: red[500],
-              },
-              [`& .${chipClasses.icon}`]: {
-                color: red[500],
-              },
-              ...theme.applyStyles('dark', {
-                borderColor: red[800],
-                backgroundColor: red[900],
-                [`& .${chipClasses.label}`]: {
-                  color: red[200],
-                },
-                [`& .${chipClasses.icon}`]: {
-                  color: red[300],
-                },
-              }),
-            },
-          },
-          {
-            props: { size: 'small' },
-            style: {
-              maxHeight: 20,
-              [`& .${chipClasses.label}`]: {
-                fontSize: theme.typography.caption.fontSize,
-              },
-              [`& .${svgIconClasses.root}`]: {
-                fontSize: theme.typography.caption.fontSize,
-              },
-            },
-          },
-          {
-            props: { size: 'medium' },
-            style: {
-              [`& .${chipClasses.label}`]: {
-                fontSize: theme.typography.caption.fontSize,
-              },
-            },
-          },
-        ],
       }),
     },
+    variants: [
+      {
+        props: {
+          color: 'default',
+        },
+        style: ({ theme }) => ({
+          borderColor: gray[200],
+          backgroundColor: gray[100],
+          [`& .${chipClasses.label}`]: {
+            color: gray[500],
+          },
+          [`& .${chipClasses.icon}`]: {
+            color: gray[500],
+          },
+          ...(theme.palette.mode === 'dark' && {
+            borderColor: gray[700],
+            backgroundColor: gray[800],
+            [`& .${chipClasses.label}`]: {
+              color: gray[300],
+            },
+            [`& .${chipClasses.icon}`]: {
+              color: gray[300],
+            },
+          }),
+        }),
+      },
+      {
+        props: {
+          color: 'success',
+        },
+        style: ({ theme }) => ({
+          borderColor: green[200],
+          backgroundColor: green[50],
+          [`& .${chipClasses.label}`]: {
+            color: green[500],
+          },
+          [`& .${chipClasses.icon}`]: {
+            color: green[500],
+          },
+          ...(theme.palette.mode === 'dark' && {
+            borderColor: green[800],
+            backgroundColor: green[900],
+            [`& .${chipClasses.label}`]: {
+              color: green[300],
+            },
+            [`& .${chipClasses.icon}`]: {
+              color: green[300],
+            },
+          }),
+        }),
+      },
+      {
+        props: {
+          color: 'error',
+        },
+        style: ({ theme }) => ({
+          borderColor: red[100],
+          backgroundColor: red[50],
+          [`& .${chipClasses.label}`]: {
+            color: red[500],
+          },
+          [`& .${chipClasses.icon}`]: {
+            color: red[500],
+          },
+          ...(theme.palette.mode === 'dark' && {
+            borderColor: red[800],
+            backgroundColor: red[900],
+            [`& .${chipClasses.label}`]: {
+              color: red[200],
+            },
+            [`& .${chipClasses.icon}`]: {
+              color: red[300],
+            },
+          }),
+        }),
+      },
+      {
+        props: { size: 'small' },
+        style: ({ theme }) => ({
+          maxHeight: 20,
+          [`& .${chipClasses.label}`]: {
+            fontSize: theme.typography.caption.fontSize,
+          },
+          [`& .${svgIconClasses.root}`]: {
+            fontSize: theme.typography.caption.fontSize,
+          },
+        }),
+      },
+      {
+        props: { size: 'medium' },
+        style: ({ theme }) => ({
+          [`& .${chipClasses.label}`]: {
+            fontSize: theme.typography.caption.fontSize,
+          },
+        }),
+      },
+    ],
   },
   MuiTablePagination: {
     styleOverrides: {
