0a0202581f603ae006c116ce44d6f575bcb754de
Eki Iman Jordiansyah <ekiimanjordiansyah212@gmail.com>
2025-05-09 21:20:58 +0700

    fixing redirect to page bankquestion list too

--- src/pages/bank/add/useBankCreateQuestionHook.js
+++ src/pages/bank/add/useBankCreateQuestionHook.js
@@ -1,10 +1,10 @@
-import {useEffect, useState} from "react";
-import useExamApi from "../../../utils/rest/exam.js";
-import {useLoading} from "../../../components/common/LoadingProvider.jsx";
-import {useModal} from "../../../components/common/ModalContext.jsx";
-import {useNavigate, useParams, useSearchParams} from "react-router";
+import { useEffect, useState } from 'react';
+import useExamApi from '../../../utils/rest/exam.js';
+import { useLoading } from '../../../components/common/LoadingProvider.jsx';
+import { useModal } from '../../../components/common/ModalContext.jsx';
+import { useNavigate, useParams, useSearchParams } from 'react-router';
 
-export function useBankCreateQuestionHook({ updatePage = false}) {
+export function useBankCreateQuestionHook({ updatePage = false }) {
   const [question, setQuestion] = useState('');
   const [optionA, setOptionA] = useState('');
   const [optionB, setOptionB] = useState('');
@@ -13,106 +13,120 @@ export function useBankCreateQuestionHook({ updatePage = false}) {
   const [optionE, setOptionE] = useState('');
   const [answer, setAnswer] = useState('');
   const [score, setScore] = useState(0);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const { id } = useParams();
   const { bankMasterCode: examCode } = useParams();
   const [searchParams] = useSearchParams();
-  const typeQuestion = searchParams.get("typeQuestion")
+  const typeQuestion = searchParams.get('typeQuestion');
   const { showLoading, hideLoading } = useLoading();
   const { showModal } = useModal();
   const [optionAnswer, setOptionAnswer] = useState([
     {
       label: 'A',
-      value: 'A'
+      value: 'A',
     },
     {
       label: 'B',
-      value: 'B'
+      value: 'B',
     },
     {
       label: 'C',
-      value: 'C'
+      value: 'C',
     },
     {
       label: 'D',
-      value: 'D'
+      value: 'D',
     },
     {
       label: 'E',
-      value: 'E'
-    }
-  ])
+      value: 'E',
+    },
+  ]);
 
   useEffect(() => {
     async function fetchData() {
       showLoading();
       if (updatePage) {
-        const { data: detailQuestion } = await useExamApi.detailBankQuestion({id: id})
-        setQuestion(detailQuestion.question)
-        detailQuestion.question_option.forEach((r,i) => {
+        const { data: detailQuestion } = await useExamApi.detailBankQuestion({
+          id: id,
+        });
+        setQuestion(detailQuestion.question);
+        detailQuestion.question_option.forEach((r, i) => {
           if (i === 0) {
-            setOptionA(r.option)
+            setOptionA(r.option);
           }
           if (i === 1) {
-            setOptionB(r.option)
+            setOptionB(r.option);
           }
           if (i === 2) {
-            setOptionC(r.option)
+            setOptionC(r.option);
           }
           if (i === 3) {
-            setOptionD(r.option)
+            setOptionD(r.option);
           }
           if (i === 4) {
-            setOptionE(r.option)
+            setOptionE(r.option);
           }
-        })
-        setAnswer(detailQuestion.answer_single)
+        });
+        setAnswer(detailQuestion.answer_single);
       }
-      hideLoading()
+      hideLoading();
     }
-    fetchData()
+    fetchData();
   }, []);
 
   const handleSubmitCreate = () => {
     const body = {
-      "answer": answer,
-      "exam_code": examCode,
-      "option_a": optionA,
-      "option_b": optionB,
-      "option_c": optionC,
-      "option_d": optionD,
-      "option_e": optionE,
-      "question": question,
-      "score": score
-    }
+      answer: answer,
+      exam_code: examCode,
+      option_a: optionA,
+      option_b: optionB,
+      option_c: optionC,
+      option_d: optionD,
+      option_e: optionE,
+      question: question,
+      score: score,
+    };
 
     showLoading();
-    useExamApi.createBankQuestion({body: body, id: id, isCreate: !updatePage}).then(r => {
-      const { message, status } = r;
-      showModal(message, status)
-      setTimeout(() => {
-        hideLoading()
-        navigate(-1)
-      }, 1500)
-    }).catch(e => {
-      console.log(e.data)
-      hideLoading()
-      showModal('Failed create Question. Please try again!', 'error')
-    })
-  }
+    useExamApi
+      .createBankQuestion({ body: body, id: id, isCreate: !updatePage })
+      .then((r) => {
+        const { message, status } = r;
+        showModal(message, status);
+        setTimeout(() => {
+          hideLoading();
+          navigate(-1);
+        }, 1500);
+      })
+      .catch((e) => {
+        console.log(e.data);
+        hideLoading();
+        showModal('Failed create Question. Please try again!', 'error');
+      });
+  };
 
   return {
-    score, setScore,
-    question, setQuestion,
-    answer, setAnswer,
-    optionA, setOptionA,
-    optionB, setOptionB,
-    optionC, setOptionC,
-    optionD, setOptionD,
-    optionE, setOptionE,
-    optionAnswer, setOptionAnswer,
+    score,
+    setScore,
+    question,
+    setQuestion,
+    answer,
+    setAnswer,
+    optionA,
+    setOptionA,
+    optionB,
+    setOptionB,
+    optionC,
+    setOptionC,
+    optionD,
+    setOptionD,
+    optionE,
+    setOptionE,
+    optionAnswer,
+    setOptionAnswer,
     typeQuestion,
     handleSubmitCreate,
-  }
+  };
 }
